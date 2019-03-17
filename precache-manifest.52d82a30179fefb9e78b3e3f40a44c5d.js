dff3b2b47918d80e03533efa3aa7423bb7758d57
Vishnu Singh <vishnu.singh@sociolla.com>
2019-03-18 01:16:35 +0530

    Updates

---
+++ precache-manifest.52d82a30179fefb9e78b3e3f40a44c5d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9be522379615270d7989fbbb07bea912",
+    "url": "/todo-react/static/media/task-in-progress.9be52237.svg"
+  },
+  {
+    "revision": "7e49425b1b7914f90f51",
+    "url": "/todo-react/static/css/main.9d29eccc.chunk.css"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  },
+  {
+    "revision": "555b87e014ba2587e9e9",
+    "url": "/todo-react/static/js/2.a08f1db4.chunk.js"
+  },
+  {
+    "revision": "e7cffb9da4786f79de819029970890e0",
+    "url": "/todo-react/static/media/task-search.e7cffb9d.svg"
+  },
+  {
+    "revision": "981ae3112abeb2825c924684168a72a0",
+    "url": "/todo-react/static/media/task-completed.981ae311.svg"
+  },
+  {
+    "revision": "2bfbf7339f2ebce75631bb77b06d8dd1",
+    "url": "/todo-react/static/media/task-pending.2bfbf733.svg"
+  },
+  {
+    "revision": "7e49425b1b7914f90f51",
+    "url": "/todo-react/static/js/main.5d11c10b.chunk.js"
+  },
+  {
+    "revision": "59c6254cab748a2e19c353fc76995780",
+    "url": "/todo-react/static/media/vishnu.59c6254c.jpg"
+  },
+  {
+    "revision": "8601f5bd9fb2d08026329c47364dde32",
+    "url": "/todo-react/static/media/plus.8601f5bd.svg"
+  },
+  {
+    "revision": "522a418bc396f1551eef594b611cab2f",
+    "url": "/todo-react/static/media/edit.522a418b.svg"
+  },
+  {
+    "revision": "c2625638b2a6db871bea29942fa946b7",
+    "url": "/todo-react/static/media/done.c2625638.svg"
+  },
+  {
+    "revision": "c94aea67d5b5de12fe2e4c44e4c187c7",
+    "url": "/todo-react/static/media/www.c94aea67.svg"
+  },
+  {
+    "revision": "8b16d0d448bac0851f636af105518439",
+    "url": "/todo-react/index.html"
+  }
+];

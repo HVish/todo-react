4182108a8e1572cb9b624d75ffc0aef589d8c171
Vishnu Singh <vishnusingh1729@gmail.com>
2019-04-08 03:34:54 +0530

    feat(side-bar): make side-nav toggleable, Closes #8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import '../styles/App.scss';
@@ -7,17 +7,25 @@ import MainContainer from './MainContainer';
 import Tasks from '../containers/Tasks';
 import { TaskStatus } from '../constants/tasks';
 
-const App = props => (
-  <div className="app">
-    <SideNav />
-    <MainContainer>
-      <Route path={`${props.match.path}/archived`} component={Tasks(TaskStatus.ARCHIVED)} />
-      <Route path={`${props.match.path}/completed`} component={Tasks(TaskStatus.COMPLETED)} />
-      <Route path={`${props.match.path}/in-progress`} component={Tasks(TaskStatus.IN_PROGRESS)} />
-      <Route path={`${props.match.path}/pending`} component={Tasks(TaskStatus.PENDING)} />
-      <Route exact path={`${props.match.path}`} component={Tasks('all')} />
-    </MainContainer>
-  </div>
-);
+const App = props => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const handleNavToggle = () => {
+    setIsNavVisible(!isNavVisible);
+  }
+
+  return (
+    <div className={isNavVisible ? "app app_nav-visible" : "app"}>
+      <SideNav isVisible={isNavVisible} onToggle={handleNavToggle} />
+      <MainContainer>
+        <Route path={`${props.match.path}/archived`} component={Tasks(TaskStatus.ARCHIVED)} />
+        <Route path={`${props.match.path}/completed`} component={Tasks(TaskStatus.COMPLETED)} />
+        <Route path={`${props.match.path}/in-progress`} component={Tasks(TaskStatus.IN_PROGRESS)} />
+        <Route path={`${props.match.path}/pending`} component={Tasks(TaskStatus.PENDING)} />
+        <Route exact path={`${props.match.path}`} component={Tasks('all')} />
+      </MainContainer>
+    </div>
+  )
+};
 
 export default App;
